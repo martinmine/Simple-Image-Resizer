3c5a5513679f07b7969cde98c17342a1a78c68be
Martin <martin_mine@hotmail.com>
2013-07-13 18:54:55 +0200

    Program data

---
+++ SimpleImageResizer/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SimpleImageResizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputKbSize = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maxHeightInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output size (KB):";
+            // 
+            // outputKbSize
+            // 
+            this.outputKbSize.Location = new System.Drawing.Point(95, 12);
+            this.outputKbSize.Name = "outputKbSize";
+            this.outputKbSize.Size = new System.Drawing.Size(58, 20);
+            this.outputKbSize.TabIndex = 4;
+            this.outputKbSize.Text = "245";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(216, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Always on top";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(12, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 72);
+            this.panel1.TabIndex = 6;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Max height (px):";
+            // 
+            // maxHeightInput
+            // 
+            this.maxHeightInput.Location = new System.Drawing.Point(95, 38);
+            this.maxHeightInput.Name = "maxHeightInput";
+            this.maxHeightInput.Size = new System.Drawing.Size(58, 20);
+            this.maxHeightInput.TabIndex = 8;
+            this.maxHeightInput.Text = "600";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Drop images here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 143);
+            this.Controls.Add(this.maxHeightInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.outputKbSize);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Simple Image Resizer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox outputKbSize;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox maxHeightInput;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SimpleImageResizer/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SimpleImageResizer
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// All the supported formats which this program can handle
+        /// </summary>
+        private static readonly string[] ACCEPTABLE_FORMATS = { "png", "jpg", "bmp" };
+        /// <summary>
+        /// The JPEG codec used to encode the output files
+        /// </summary>
+        private ImageCodecInfo jpegCodec;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetCodec();
+            
+            this.checkBox1.Checked = true;  // Make this window top-most by default
+        }
+        
+        /// <summary>
+        /// Checks if the file string is a supported format
+        /// </summary>
+        /// <param name="file">Full path to an image</param>
+        /// <returns>True if support, false otherwise</returns>
+        private static bool isValidFormat(string file)
+        {
+            for (int i = 0; i < ACCEPTABLE_FORMATS.Length; i++)
+                if (file.EndsWith(ACCEPTABLE_FORMATS[i]))
+                    return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Event called when the topmost checkbox is checked in the UI
+        /// </summary>
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = checkBox1.Checked;
+        }
+
+        /// <summary>
+        /// Enables the small icon which indicates you can drop an item on the blue box
+        /// </summary>
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        /// <summary>
+        /// Event called once an item has been dropped in the drop area
+        /// </summary>
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            foreach (string file in files)
+            {
+                if (isValidFormat(file.ToLower()))
+                    handleImage(file);
+                else
+                    MessageBox.Show("The file format of this file is unknown or not supported");
+            }
+        }
+
+        /// <summary>
+        /// Compresses and resize an image
+        /// </summary>
+        /// <param name="path">Path to the image</param>
+        private void handleImage(string path)
+        {
+            Image img = Image.FromFile(path);   // Load image
+            int quality = 100;  // Quality of the image
+            int size = 0;       // The file size of the image
+            int maxHeight;      // Maximum allowed height on the image
+            int maxSize;        // Maximum allowed file size on the image
+
+            string newPath = null;  // Path to the compressed/resized image
+
+            if (!int.TryParse(outputKbSize.Text, out maxSize))
+            {
+                MessageBox.Show("Invalid text input size, must be numbers only!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!int.TryParse(maxHeightInput.Text, out maxHeight))
+            {
+                MessageBox.Show("Invalid max height, must be numbers only!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (img.Size.Height > maxHeight)    // Resize image if needed
+            {
+                int newHeight = maxHeight;
+                int newWidth = (int)((float)img.Size.Width * ((float)newHeight / (float)img.Size.Height));
+                resizeImage(ref img, new Size(newWidth, newHeight));
+            }
+
+            do
+            {
+                if (!string.IsNullOrEmpty(newPath))
+                    File.Delete(newPath);
+
+                newPath = path.Insert(path.Length - 4, " compressed");
+                CompressImage(img, quality, newPath);
+
+                FileInfo fileInfo = new FileInfo(newPath);  // Get the size of the output file
+                size = (int)(fileInfo.Length / 1024);
+
+                quality -= 10;  // Decrease compression rate with 10 at each try
+            }
+            while (size > maxSize && quality > 0);  // While compression rate is > 0 and size is above max size
+
+            if (size > maxSize)
+            {
+                MessageBox.Show(string.Format("Unable to create an image as small as {0} kb for image {1}. Please reduce the output kb limit.", maxSize, path), 
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            img.Dispose();
+        }
+
+        /// <summary>
+        /// Sets the JPEG codec, called at program startup
+        /// </summary>
+        private void SetCodec()
+        {
+            //List all avaible codecs (system wide)
+            ImageCodecInfo[] alleCodecs = ImageCodecInfo.GetImageEncoders();
+
+            //Find and choose JPEG codec
+            for (int i = 0; i < alleCodecs.Length && jpegCodec == null; i++)
+                if (alleCodecs[i].MimeType == "image/jpeg")
+                    jpegCodec = alleCodecs[i];
+        }
+
+        /// <summary>
+        /// Compresses the image and saves the image
+        /// </summary>
+        /// <param name="sourceImage">The image to resize</param>
+        /// <param name="imageQuality">Compression rate</param>
+        /// <param name="savePath">Path to where the image shall be saved</param>
+        private void CompressImage(Image sourceImage, int imageQuality, string savePath)
+        {
+            //Set quality factor for compression
+            EncoderParameter imageQualitysParameter = new EncoderParameter(
+                        System.Drawing.Imaging.Encoder.Quality, imageQuality);
+
+            EncoderParameters codecParameter = new EncoderParameters(1);
+            codecParameter.Param[0] = imageQualitysParameter;
+
+            //Save compressed image
+            sourceImage.Save(savePath, jpegCodec, codecParameter);
+        }
+
+        /// <summary>
+        /// Resizes the image in height and width
+        /// </summary>
+        /// <param name="imgToResize">Referance to the image to resize</param>
+        /// <param name="size">The size of the image</param>
+        private static void resizeImage(ref Image imgToResize, Size size)
+        {
+            int sourceWidth = imgToResize.Width;
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            using (Graphics g = Graphics.FromImage((Image)b))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            }
+            
+            imgToResize.Dispose();
+            imgToResize = (Image)b;
+        }
+    }
+}
